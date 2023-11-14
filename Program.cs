c11d636ebc3bf63e1fb2f12ce84ac009104a5e0f
alesf9910 <alesf9910@gmail.com>
2023-11-13 22:32:55 -0500

    escaner de puertos

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPAddress address = IPAddress.Parse(args[0]);
+
+int min = int.Parse(args[1]);
+int max = int.Parse(args[2]);
+
+List<Task> tasks = new List<Task>();
+
+for (int i = min; i <= max; i++)
+{
+    int port = i;
+    tasks.Add(Task.Run(() =>
+    {
+        try{
+            TcpClient client = new TcpClient();
+            client.Connect(address, port);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Puerto {0} esta abierto", port);
+        }catch{
+            Console.ResetColor();
+            Console.WriteLine("Puerto {0} esta cerrado", port);
+        }
+    }));
+}
+
+Task.WaitAll(tasks.ToArray());
